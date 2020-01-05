db90b2bdd87ef2512e9c35ffc616404755e31b97
JontePonte <john_nordstrand@hotmail.com>
2020-01-05 10:18:34 +0100

    Created files and updated readme

---
+++ Input.py
@@ -0,0 +1,16 @@
+
+"""
+Input all your values here.
+"""
+
+# Growth of savings every year
+YEARLY_INTEREST = 0.10
+
+# Monthly depositions (in sek)
+MONTHLY_DEP = 2000
+
+# Money in account from start
+START_MONEY = 50000
+
+# Calculated time period in years
+TIME_YEARS = 10
